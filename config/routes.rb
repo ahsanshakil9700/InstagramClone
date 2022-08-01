4cda8d7cc65a815497c2ea2dee9c97865fae3bac
ahsanshakil9700 <ahsan.shakil@devsinc.com>
2022-08-02 02:26:57 +0500

    Created Views for Edit profile and Update profile, these are dummy views. next pr will be post module 1.2 that contains image upload functionality

--- config/routes.rb
+++ config/routes.rb
@@ -22,13 +22,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
 
-  resources :accounts, only: [:show, :index]
+  resources :accounts, only: [:show, :index, :edit, :update]
 
   # dashboard
   get '/dashboard', to: 'accounts#index'
-
-
-
-
 
 end
